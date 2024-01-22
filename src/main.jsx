af4178da570df5fe1a207cf384f8af0fd5e6a32e
Teslyak <124871929+Teslyak@users.noreply.github.com>
2024-01-22 15:10:10 +0200

    fix package.json homepage

--- src/main.jsx
+++ src/main.jsx
@@ -4,14 +4,14 @@ import App from "./App.jsx";
 import "./index.css";
 import { BrowserRouter } from "react-router-dom";
 import { Provider } from "react-redux";
-import { PersistGate } from "redux-persist/integration/react";
+//import { PersistGate } from "redux-persist/integration/react";
 import { store } from "./redux/store.jsx";
 
 ReactDOM.createRoot(document.getElementById("root")).render(
   <React.StrictMode>
     <Provider store={store}>
       {/* <PersistGate loading={null} persistor={persistor}></PersistGate> */}
-      <BrowserRouter basename="/rental-cars/">
+      <BrowserRouter basename="/rental-cars">
         <App />
       </BrowserRouter>
       {/* <PersistGate /> */}
