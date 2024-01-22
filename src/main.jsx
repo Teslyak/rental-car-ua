f9aabca7a757584996cf7c9b43987728530dce0f
Teslyak <124871929+Teslyak@users.noreply.github.com>
2024-01-22 20:03:45 +0200

    fix homepage

--- src/main.jsx
+++ src/main.jsx
@@ -11,7 +11,7 @@ ReactDOM.createRoot(document.getElementById("root")).render(
   <React.StrictMode>
     <Provider store={store}>
       {/* <PersistGate loading={null} persistor={persistor}></PersistGate> */}
-      <BrowserRouter basename="/rental-cars">
+      <BrowserRouter basename="/rental-car-ua">
         <App />
       </BrowserRouter>
       {/* <PersistGate /> */}
