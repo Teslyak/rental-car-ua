6db78c9644679f04af0d33f79fc045f9c277d778
Teslyak <124871929+Teslyak@users.noreply.github.com>
2024-01-23 15:37:32 +0200

    fix main

--- src/main.jsx
+++ src/main.jsx
@@ -4,17 +4,17 @@ import App from "./App.jsx";
 import "./index.css";
 import { BrowserRouter } from "react-router-dom";
 import { Provider } from "react-redux";
-//import { PersistGate } from "redux-persist/integration/react";
-import { store } from "./redux/store.jsx";
+import { PersistGate } from "redux-persist/integration/react";
+import { persistor, store } from "./redux/store";
 
 ReactDOM.createRoot(document.getElementById("root")).render(
   <React.StrictMode>
     <Provider store={store}>
-      {/* <PersistGate loading={null} persistor={persistor}></PersistGate> */}
-      <BrowserRouter basename="/rental-car-ua/">
-        <App />
-      </BrowserRouter>
-      {/* <PersistGate /> */}
+      <PersistGate loading={null} persistor={persistor}>
+        <BrowserRouter basename="/rental-car-ua">
+          <App />
+        </BrowserRouter>
+      </PersistGate>
     </Provider>
   </React.StrictMode>
 );
